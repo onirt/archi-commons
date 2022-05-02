28eb7299ee1cbe69fbc6155b7476a435adf6098a
Trino Parra <onirt@hotmail.com>
2022-05-02 21:23:30 +0200

    inpu controls

--- Scripts/AI/AIBehaviour.cs
+++ Scripts/AI/AIBehaviour.cs
@@ -6,8 +6,13 @@ public class AIBehaviour : MonoBehaviour, IAIBehaviour
 {
     [SerializeField] private Animator animator;
 
+    private void Start()
+    {
+        animator.SetBool("Started", true);
+    }
     public void Init()
     {
+        Debug.Log("[AIBehaviour] Init");
         animator.SetBool("Started", true);
     }
 
